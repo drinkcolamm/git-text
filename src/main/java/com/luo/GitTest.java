f76b51b0c3952d28c6bd80f10d49df3f5a4b1945
luolurong <luolurong@126.com>
2024-02-22 05:37:24 +0800

    hot-fix commit 2

--- src/main/java/com/luo/GitTest.java
+++ src/main/java/com/luo/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("helloGit");
         System.out.println("helloGit2");
         System.out.println("helloGit3");
+        System.out.println("hot-fix test");
     }
 }
