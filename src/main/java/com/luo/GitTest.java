25ebd99ce77ec80706e3f10b8ef6a912c91f0a49
luolurong <luolurong@126.com>
2024-02-23 09:12:41 +0800

    push commit

--- src/main/java/com/luo/GitTest.java
+++ src/main/java/com/luo/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("helloGit3");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
